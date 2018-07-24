12e5f9e67b001f8f388d60db0912817101320a60
Viktor H <viktorhegyi@gmail.com>
2018-07-24 10:28:15 +0200

    add the app

---
+++ src/BookSearch.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react'
+import { Link } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
+import "./App.css";
+
+class BookSearch extends Component {
+
+  state = {
+    query: '',
+    books: [],
+  }
+
+  updateData = (books) => {
+    const curBooks = books.map(book => {
+      book.shelf = "none";
+      this.props.currentBooks.forEach(book2add => {
+        if (book.id === book2add.id) {
+          book.shelf = book2add.shelf;
+        }
+      })
+      return book;
+    })
+    this.setState({
+      books: curBooks
+    })
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query })
+    if (query) {
+      BooksAPI.search(query).then((books) => {
+        books.length > 0 ? this.updateData(books) : this.setState({books:[]})
+      }).catch((e)=> {
+      console.error(`API error: ${e}`);
+    })
+    }
+    else
+    {this.setState({books:[]})}
+  }
+
+  updateBooks = (book, shelf)=> {
+    let current = this.state.books;
+    const bookToUpdate = current.filter(curBook => curBook.id === book.id)[0];
+    bookToUpdate.shelf = shelf;
+    this.setState({
+      books: current
+    })
+    this.props.updateShelf(book, shelf);
+  }
+
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/" className="close-search">
+            Close
+          </Link>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={event => this.updateQuery(event.target.value)}
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {this.state.books.filter((book) => (book.imageLinks)).map(book =>
+              <li key={book.id} className="book">
+                <div className="book-top">
+                  <div
+                    className="book-cover"
+                    style={{width: 128, height: 193,
+                      backgroundImage: "url(" + book.imageLinks.thumbnail + ")"
+                    }}
+                  />
+                  <div className="book-shelf-changer">
+                    <select
+                      value={book.shelf}
+                      onChange={e => {
+                        this.updateBooks(book, e.target.value);
+                      }}>
+                      <option disabled>Move to...</option>
+                      <option value="currentlyReading">Currently Reading</option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="book-title">
+                  {book.title}
+                </div>
+                <div className="book-authors">
+                  {book.authors}
+                </div>
+              </li>
+            )}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+export default BookSearch;
